2dddfc292f095656bda372d4eb85727da1872ea4
ht3aa <ht3aa2001@gmail.com>
2023-01-03 15:13:33 +0300

    udding html file

--- macNavAnimation.js
+++ macNavAnimation.js
@@ -9,40 +9,43 @@ const childSmallestValue = 50;
 //---------------------------------------------
 
 function getChildrenXCenterValues() {
-    for(let i = 0; i < childernOfparentDiv.length; i++){
-        let childernXValue = Math.floor(childernOfparentDiv[i].getBoundingClientRect().x);
-        let childernWidth = Math.floor(childernOfparentDiv[i].getBoundingClientRect().width);
-        childrenXCenterValues.push((childernXValue + childernWidth / 2) / 100);
-    }
+  for (let i = 0; i < childernOfparentDiv.length; i++) {
+    let childernXValue = Math.floor(
+      childernOfparentDiv[i].getBoundingClientRect().x
+    );
+    let childernWidth = Math.floor(
+      childernOfparentDiv[i].getBoundingClientRect().width
+    );
+    childrenXCenterValues.push((childernXValue + childernWidth / 2) / 100);
+  }
 }
 getChildrenXCenterValues();
 
-
-function move(index, y){
-    childernOfparentDiv[index].style.width = `${y}px`;
-    childernOfparentDiv[index].style.height = `${y}px`;
+function move(index, y) {
+  childernOfparentDiv[index].style.width = `${y}px`;
+  childernOfparentDiv[index].style.height = `${y}px`;
 }
-function reset(index){
-    childernOfparentDiv[index].style.width = `${childSmallestValue}px`;
-    childernOfparentDiv[index].style.height = `${childSmallestValue}px`;
+function reset(index) {
+  childernOfparentDiv[index].style.width = `${childSmallestValue}px`;
+  childernOfparentDiv[index].style.height = `${childSmallestValue}px`;
 }
 
+parentDiv.addEventListener("mousemove", function (e) {
+  let xMouse = parseFloat(e.clientX) / 100;
 
-parentDiv.addEventListener("mousemove", function(e) {
-    let xMouse = parseFloat(e.clientX) / 100;
+  for (let i = 0; i < childernOfparentDiv.length; i++) {
+    let y =
+      amplifier *
+      Math.sqrt(range - Math.pow(xMouse - childrenXCenterValues[i], 2));
 
-    for(let i = 0; i < childernOfparentDiv.length; i++) {
-        let y = amplifier * Math.sqrt(range - (Math.pow(xMouse - childrenXCenterValues[i], 2)));
+    y > childSmallestValue ? move(i, y) : reset(i);
+  }
+});
 
-        y > childSmallestValue ? move(i, y) : reset(i);
-    }
-})
-  
-parentDiv.addEventListener("mouseleave", function() {
-    for(let i = 0; i < childernOfparentDiv.length; i++) {
-        reset(i);
-    }
-})
-
+parentDiv.addEventListener("mouseleave", function () {
+  for (let i = 0; i < childernOfparentDiv.length; i++) {
+    reset(i);
+  }
+});
 
 // Made by hassan tahseen - instegram,telegram: @ht3aa
